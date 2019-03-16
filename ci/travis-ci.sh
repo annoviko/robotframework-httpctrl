93a7a65db4ca434861d167f2183554e6fea83cde
annoviko <spb.andr@yandex.ru>
2019-03-16 19:45:25 +0300

    [ci] Added debug information to CI process.

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -1,19 +1,26 @@
 # package to test
+echo "Install Robot Framework."
 sudo pip3 install -q robotframework
 
 # packages to generate documentation
+echo "Install packages to generate documentation."
 sudo pip3 install -q docutils pygments
 
 # export path to the library
+echo "Export path to the HttpCtrl library."
 cd src
 PYTHONPATH=`pwd`
 export PYTHONPATH=${PYTHONPATH}
 
+echo "Path '$PYTHONPATH' is exported."
+
 # run tests
+echo "Run tests for HttpCtrl."
 cd ../tst
 python3 -m robot *.robot
 
 # generate documentation
+echo "Generate documentation for HttpCtrl."
 cd ../src
 python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Client client.html
 python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Server server.html
