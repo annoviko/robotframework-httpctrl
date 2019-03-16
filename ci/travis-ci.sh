26da983833d5a7513f7535687e08e192fd9eba6b
annoviko <spb.andr@yandex.ru>
2019-03-16 19:19:09 +0300

    [HttpCtrl] Add version to CI script.

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -10,6 +10,6 @@ python3 -m robot *.robot
 
 # generate documentation
 cd ../src
-python3 -m robot.libdoc -F reST HttpCtrl.Client client.html
-python3 -m robot.libdoc -F reST HttpCtrl.Server server.html
-python3 -m robot.libdoc -F reST HttpCtrl.Json json.html
+python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Client client.html
+python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Server server.html
+python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Json json.html
