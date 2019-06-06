fd889c7b6a04975dd1d36a61fcfd6b9a8e60eb7a
annoviko <spb.andr@yandex.ru>
2019-06-06 16:00:23 +0300

    [ci] Try to make CI workable after Travis-CI update.

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -1,10 +1,10 @@
 # package to test
 echo "Install Robot Framework."
-sudo pip3 install -q robotframework-python3
+sudo pip install -q robotframework-python3
 
 # packages to generate documentation
 echo "Install packages to generate documentation."
-sudo pip3 install -q docutils pygments
+sudo pip install -q docutils pygments
 
 # export path to the library
 echo "Export path to the HttpCtrl library."
@@ -17,11 +17,11 @@ echo "Path '$PYTHONPATH' is exported."
 # run tests
 echo "Run tests for HttpCtrl."
 cd ../tst
-python3 -m robot *.robot
+python -m robot *.robot
 
 # generate documentation
 echo "Generate documentation for HttpCtrl."
 cd ../src
-python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Client client.html
-python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Server server.html
-python3 -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Json json.html
+python -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Client client.html
+python -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Server server.html
+python -m robot.libdoc -v 0.1.0 -F reST HttpCtrl.Json json.html
