c2911572f893740b795f318a185b1ef4c7f8752c
annoviko <spb.andr@yandex.ru>
2019-09-07 22:04:55 +0200

    #9: [ci] Travis-CI marks build as successful in case of errors.

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -1,3 +1,7 @@
+# error codes
+EXIT_CODE_INCORRECT_PATH=1
+EXIT_CODE_FAILED_TESTING=2
+
 # package to test
 echo "Install Robot Framework."
 pip3 install robotframework
@@ -8,7 +12,7 @@ pip3 install docutils pygments
 
 # export path to the library
 echo "Export path to the HttpCtrl library."
-cd src
+cd src || exit $EXIT_CODE_INCORRECT_PATH
 PYTHONPATH=`pwd`
 export PYTHONPATH=${PYTHONPATH}
 
@@ -16,12 +20,20 @@ echo "Path '$PYTHONPATH' is exported."
 
 # run tests
 echo "Run tests for HttpCtrl."
-cd ../tst
+cd ../tst || exit $EXIT_CODE_INCORRECT_PATH
 python3 -m robot.run *.robot
+
+# check testing results
+result=$?
+if [ $result -ne 0 ]
+then
+  echo "Testing results contain errors - mark build as a failure."
+  exit $EXIT_CODE_FAILED_TESTING
+fi
 
 # generate documentation
 echo "Generate documentation for HttpCtrl."
-cd ../src
+cd ../src || exit $EXIT_CODE_INCORRECT_PATH
 python3 -m robot.libdoc -v 0.1.2 -F reST HttpCtrl.Client client.html
 python3 -m robot.libdoc -v 0.1.2 -F reST HttpCtrl.Server server.html
 python3 -m robot.libdoc -v 0.1.2 -F reST HttpCtrl.Json json.html
