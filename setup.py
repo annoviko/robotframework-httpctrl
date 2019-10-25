fcd59121968d17b60e70065dd9b4eeb84632f72a
annoviko <spb.andr@yandex.ru>
2019-10-25 10:30:27 +0200

    [.] Release preparation (0.1.3).

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def load_readme():
 setup(
     name='robotframework-httpctrl',
     packages=['HttpCtrl'],
-    version='0.1.2',
+    version='0.1.3',
     description='robotframework-httpctrl is a library for Robot Framework that provides HTTP/HTTPS client and HTTP server services',
     platforms='any',
     long_description=load_readme(),
