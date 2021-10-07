4177bf9c1cb2d5d3543f95085e6961e2c3167efc
annoviko <spb.andr@yandex.ru>
2021-10-07 14:15:10 +0200

    #14: Increment version and upload documentation.

--- ci/github-ci-release.sh
+++ ci/github-ci-release.sh
@@ -2,9 +2,29 @@ PATH_ROOT=`readlink -f ..`
 PATH_REPO_CURRENT=`readlink -f .`
 PATH_SOURCE=`readlink -f src`
 
+
+# increment release
+version=`cat VERSION`
+pattern="([0-9]+).([0-9]+).([0-9]+)"
+if [[ $version ~= $pattern ]]
+then
+    major="${BASH_REMATCH[1]}"
+    minor="${BASH_REMATCH[2]}"
+    micro="${BASH_REMATCH[3]}"
+
+    micro=$((micro + 1))
+else
+    echo "Impossible to extract version to make release"
+    return -1
+fi
+
+echo "$major.$minor.$micro" > VERSION
+
+
 # packages to generate documentation
 echo "Install packages for release."
 pip3 install robotframework docutils pygments twine
+
 
 # Release to PyPi
 python3 setup.py sdist
@@ -13,6 +33,7 @@ if [ $? -ne 0 ]; then
     echo "Impossible to release the library to PyPi."
     return -1
 fi
+
 
 # Create folder for the documentation
 echo "Clone repository to update documentation."
@@ -25,17 +46,19 @@ git checkout gh-pages
 
 PATH_REPO_GH_PAGES=`readlink -f .`
 
+
 # Generate documentation
 echo "Generate documentation."
 
 cd $PATH_REPO_CURRENT
-version=`cat config.txt`
+version=`cat VERSION`
 
 cd $PATH_SOURCE
 
 python3 -m robot.libdoc -v $version -F reST HttpCtrl.Client $PATH_REPO_GH_PAGES/client.html
 python3 -m robot.libdoc -v $version -F reST HttpCtrl.Server $PATH_REPO_GH_PAGES/server.html
 python3 -m robot.libdoc -v $version -F reST HttpCtrl.Json $PATH_REPO_GH_PAGES/json.html
+
 
 # Commit and push documentation changes
 echo "Upload the documentation."
