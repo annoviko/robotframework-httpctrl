40cfe9add35b20cd5018969fdf223c330e0a4e84
annoviko <spb.andr@yandex.ru>
2021-10-07 11:53:41 +0200

    #14: Update ci script to make release.

--- ci/github-ci-release.sh
+++ ci/github-ci-release.sh
@@ -10,17 +10,25 @@ pip3 install robotframework docutils pygments twine
 # Release to PyPi
 python3 setup.py sdist
 twine upload dist/* -r testpypi
+if [ $? -ne 0 ]; then
+    echo "Impossible to release the library to PyPi."
+    return -1
+fi
 
 # Create folder for the documentation
+echo "Clone repository to update documentation."
+
 mkdir $PATH_ROOT/httpctrl-gh-pages
 cd $PATH_ROOT/httpctrl-gh-pages
 
 git clone https://github.com/annoviko/robotframework-httpctrl.git .
-git checkout hg-pages
+git checkout gh-pages
 
 PATH_REPO_GH_PAGES=`readlink -f .`
 
 # Generate documentation
+echo "Generate documentation."
+
 cd $PATH_REPO_CURRENT
 version=`cat config.txt`
 
@@ -31,6 +39,8 @@ python3 -m robot.libdoc -v $version -F reST HttpCtrl.Server $PATH_REPO_GH_PAGES/
 python3 -m robot.libdoc -v $version -F reST HttpCtrl.Json $PATH_REPO_GH_PAGES/json.html
 
 # Commit and push documentation changes
+echo "Upload the documentation."
+
 cd $PATH_REPO_GH_PAGES
 
 git push https://$HTTPCTRL_USERNAME:$HTTPCTRL_PASSWORD@github.com/annoviko/robotframework-httpctrl.git --all
